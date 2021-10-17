eae42cd65f6f8f242f8635fa85ad5742b1aba4ca
Nilesh1452000 <92640453+Nilesh1452000@users.noreply.github.com>
2021-10-17 22:40:46 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,8 +6,8 @@ import com.wordpress.technologyconversations.tdd.StringCalculator;
  
 public class StringCalculatorTest {
   @Test
-    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
-        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
-    }
+public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
+    Assert.assertEquals(3+6+15, StringCalculator.add("3,6n15"));
+}
     }
 
