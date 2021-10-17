d614199f5cd1e170f7eff9c52b8c33c7617db7ed
Nilesh1452000 <92640453+Nilesh1452000@users.noreply.github.com>
2021-10-17 22:30:05 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,21 @@
 package calculator;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class StringCalculatorShould {
-
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+package com.wordpress.technologyconversations.tddtest;
+ 
+import org.junit.Test;
+import com.wordpress.technologyconversations.tdd.StringCalculator;
+ 
+public class StringCalculatorTest {
+    @Test(expected = RuntimeException.class)
+    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,2,3");
     }
-
     @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        StringCalculator.add("1,2");
+        Assert.assertTrue(true);
+    }
+    @Test(expected = RuntimeException.class)
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,X");
     }
 }
